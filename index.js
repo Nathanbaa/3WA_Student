50405414c6062fa64ca51a8001e04593e9dd37df
Nathanbaa <contact.nathanbastard@gmail.com>
2024-03-04 10:45:12 +0100

    correction clg

--- index.js
+++ index.js
@@ -3,29 +3,7 @@ import chalk from "chalk";
 // test nodemon
 // console.log("Nodemon is running");
 
-const colleagues = ["Nathan", "Elisa", "Jason", "Marianne"];
-
-function displayColleagues() {
-  colleagues.forEach((name, index) => {
-    let color;
-    switch (index % 4) {
-      case 0:
-        color = chalk.blue;
-        break;
-      case 1:
-        color = chalk.green;
-        break;
-      case 2:
-        color = chalk.yellow;
-        break;
-      case 3:
-        color = chalk.red;
-        break;
-      default:
-        color = chalk.white;
-    }
-    console.log(color(name));
-  });
-}
-
-displayColleagues();
+console.log(chalk.blue("Nathan"));
+console.log(chalk.red("Elisa"));
+console.log(chalk.yellow("Marianne"));
+console.log(chalk.green("Jason"));
